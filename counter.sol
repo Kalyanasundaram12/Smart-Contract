9ccc855948dea90371ca90df5cef248fb4236566
Kalyanasundaram V <112867527+Kalyanasundaram12@users.noreply.github.com>
2023-02-27 21:34:23 +0530

    Create counter.sol

---
+++ counter.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier:unlicensed
+pragma solidity ^0.8.9;
+
+contract counter{
+    int count ;
+    constructor(){
+        count=1;
+
+    }
+    event Incremented(int value);
+    event Decremented(int value);
+
+    function getValue() view public returns(int){
+        return count;
+    }
+
+    function Increment() public{
+        count += 1;
+        emit Incremented(count);
+    } 
+     function Decrement() public{
+        count -= 1;
+        emit Decremented(count);
+    } 
+
+
+}
